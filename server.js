55d4f6572ceb27427205bb96e0883b9ca3c90553
Sassoun Derderian <sassounderderian@workfront.com>
2020-07-24 13:04:28 +0400

    feat: show average confirmed cases
    Introduced chart.js. Showing graphs for # of tests, # of confirmed cases and % of average confirmed cases

--- server.js
+++ server.js
@@ -10,50 +10,67 @@ const { JSDOM } = jsdom;
 
 app.set("view engine", "pug");
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  const dom = JSDOM.fromURL(
+const fetchData = () => JSDOM.fromURL(
     "https://e.infogram.com/71d42d9d-504e-4a8b-a697-f52f4178b329?src=embed",
-    { resources: "usable", runScripts: "dangerously" }
+    { runScripts: "dangerously" }
   )
-    .then(
-      dom => 
-        new Promise(resolve => {
-          dom.window.onload = () => resolve(dom);
-        })
-    )
+    .then(dom => new Promise(r => dom.window.onload = () => r(dom)))
     .then(dom => {
       const data =
         dom.window.infographicData.elements.content.content.entities[
           "6c2332a5-b0ba-458e-aedf-21b9ec2a9722"
         ].props.chartData.data[0];
       data.shift();
-      // const confirmedRelativeToTests = []
-      const tests = [];
-      const confirmedCases = [];
-      data.map(([dateStr, confirmed, , negativeTests]) => {
-        const date = new Date(
-          dateStr
-            .split(".")
-            .reverse()
-            .join("-")
-        );
-
-        confirmed = parseInt(confirmed);
-        negativeTests = parseInt(negativeTests);
-        const totalTested = negativeTests + confirmed;
-
-        // const confirmedPercentage = confirmed ? confirmed*100 / totalTested : 0
-        confirmedCases.push({ x: dateStr, value: confirmed });
-        tests.push({ x: dateStr, value: totalTested });
-      });
-
-      response.render("index", { confirmedCases, tests });
+      return data.map(([dateStr, ...rest]) => [
+          dateStr.split(".").reverse().join("-"),
+          ...rest.map(Number)
+        ]
+      )
     })
-    .catch(e => {
-      response.render("index", { confirmedCases: [], tests: [] });
-    });
-});
+
+
+// https://expressjs.com/en/starter/basic-routing.html
+// app.get("/", (request, response) => {
+//   fetchData().then(data => {
+//       // const confirmedRelativeToTests = []
+//       const tests = [];
+//       const confirmedCases = [];
+//       data.map(([dateStr, confirmed, , negativeTests]) => {
+//         const date = new Date(
+//           dateStr
+//             .split(".")
+//             .reverse()
+//             .join("-")
+//         );
+
+//         confirmed = parseInt(confirmed);
+//         negativeTests = parseInt(negativeTests);
+//         const totalTested = negativeTests + confirmed;
+
+//         // const confirmedPercentage = confirmed ? confirmed*100 / totalTested : 0
+//         confirmedCases.push({ x: dateStr, value: confirmed });
+//         tests.push({ x: dateStr, value: totalTested });
+//       });
+
+//       response.render("index", { confirmedCases, tests });
+//     })
+//     .catch(e => {
+//       response.render("index", { confirmedCases: [], tests: [] });
+//     });
+// });
+
+
+app.get("/", (request, response) => {
+    response.render("chart")
+})
+
+app.get('/data', (req, res, next) => {
+  fetchData().then(data => {
+    res.set({'Cache-Control': 'public, max-age=3600'})
+    res.json(data)
+    next()
+  })
+})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
